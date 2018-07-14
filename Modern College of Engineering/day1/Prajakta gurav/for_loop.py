e745c8c5646ab2783578f84b245a9b18011a9999
Prajakta Gurav <prajaktagurav1914@gmail.com>
2018-07-14 05:02:23 -0700

    Day1_Pythonprograms

---
+++ Modern College of Engineering/day1/Prajakta gurav/for_loop.py
@@ -0,0 +1,7 @@
+def print_hundred_number() :
+    for i in range(1,100):
+      print(i)
+    
+
+print_hundred_number()
+
---
+++ Modern College of Engineering/day1/Prajakta gurav/triangle.py
@@ -0,0 +1,12 @@
+
+def area(b,h):
+  area=0.5*b*h
+  print("area is {}".format(area))
+
+area(2,3)
+
+
+
+
+
+      
